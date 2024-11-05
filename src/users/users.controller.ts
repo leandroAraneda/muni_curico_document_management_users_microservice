1d5794b28f4e6bde92b9c0a4067e46c1f4a7c677
Luis Araneda <leandro.araneda@hotmail.com>
2024-11-05 01:56:35 -0300

    feat: findImage and updateUser are added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,36 +2,52 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { UsersService } from './users.service';
-
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
-  @MessagePattern({ cmd: 'createUser' })
-  create(@Payload() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @MessagePattern({ cmd: 'findAllUsers' })
   findAll() {
     return this.usersService.findAll();
   }
 
-  // @MessagePattern('findOneUser')
-  // findOne(@Payload() id: number) {
-  //   return this.usersService.findOne(id);
-  // }
+  @MessagePattern({ cmd: 'findUsers' })
+  findUser(@Payload() id: string) {
+    return this.usersService.findUser(id);
+  }
 
-  // @MessagePattern('updateUser')
-  // update(@Payload() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(updateUserDto.id, updateUserDto);
-  // }
+  @MessagePattern({ cmd: 'findImage' })
+  findImage(@Payload() payload: { type: string, fileName: string }) {
+    const { type, fileName } = payload;
+    return this.usersService.findImage(type, fileName);
+  }
 
-  // @MessagePattern('removeUser')
-  // remove(@Payload() id: number) {
-  //   return this.usersService.remove(id);
-  // }
+  @MessagePattern({ cmd: 'createUser' })
+  create(@Payload() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @MessagePattern({ cmd: 'updateUser' })
+  update(@Payload() payload: { id: string, type: string, file: { originalname: string, buffer: string } }) {
+    const { id, type, file } = payload;
+    const buffer = Buffer.from(file.buffer, 'base64');
+
+    const fileToSave: Express.Multer.File = {
+      fieldname: 'file', // name of the field containing the file
+      originalname: file.originalname,
+      encoding: '7bit',
+      mimetype: 'application/octet-stream',
+      buffer: buffer,
+      size: buffer.length,
+      stream: null,
+      destination: '',
+      filename: '',
+      path: '',
+    };
+
+    return this.usersService.update(id, type, fileToSave);
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-
+import { RpcException } from '@nestjs/microservices';
+import { JwtService } from '@nestjs/jwt';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as bcrypt from 'bcrypt';
+const { v4: uuidv4 } = require('uuid');
 import { PrismaClient } from '@prisma/client';
 
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @Injectable()
 export class UsersService extends PrismaClient implements OnModuleInit {
@@ -14,39 +19,333 @@ export class UsersService extends PrismaClient implements OnModuleInit {
     this.logger.log('Connected to the database');
   }
 
-  async create(createUserDto: CreateUserDto) {
-    const resp = await this.users.create({
-      data: createUserDto,
-    });
+  constructor(private readonly jwtService: JwtService) {
+    super();
+  }
 
-    const resp_data = {
-      ...resp,
-      roleId: resp.roleId.toString(),
-    };
-
-    return resp_data;
+  async singJWT(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 
   async findAll() {
-    const users = await this.users.findMany();
+    try {
+      const users = await this.users.findMany({
+        select: {
+          id: true,
+          rut: true,
+          first_name: true,
+          second_name: true,
+          paternal_lastname: true,
+          maternal_lastname: true,
+          email: true,
+          creator_user: true,
+          status: true,
+          createdAt: true,
+          updatedAt: true,
+          roles: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
 
-    const users_data = users.map(user => ({
+      if (!users) {
+        throw new RpcException({
+          status: 204,
+          message: 'No user data.',
+        });
+      }
+
+      const users_data = users.map(user => this.convertBigInts({
+        ...user,
+        roles: user.roles ? {
+          ...user.roles,
+          id: user.roles.id ? user.roles.id.toString() : null,
+        } : null,
+      }));
+
+      return users_data;
+    } catch (error) {
+      throw new RpcException({
+        status: 500,
+        message: error.message,
+      });
+    }
+  }
+
+  async findUser(id: string) {
+    const user = await this.users.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        rut: true,
+        first_name: true,
+        second_name: true,
+        paternal_lastname: true,
+        maternal_lastname: true,
+        email: true,
+        creator_user: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+        roles: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      throw new RpcException({
+        status: 204,
+        message: `There are no users with the d: ${id}`,
+      });
+    }
+
+    const user_data = this.convertBigInts({
       ...user,
-      roleId: user.roleId ? user.roleId.toString() : null,
-    }));
+      roles: user.roles ? {
+        ...user.roles,
+        id: user.roles.id ? user.roles.id.toString() : null,
+      } : null,
+    });
 
-    return users_data;
+    return user_data;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findImage(type: string, fileName: string) {
+    try {
+      const validImageTypes = ['users', 'persons'];
+
+      if (!validImageTypes.includes(type)) {
+        throw new RpcException({
+          status: 400,
+          message: 'Invalid image type.',
+        });
+      }
+
+      let pathImage = path.join(process.cwd(), 'dist', 'uploads', type, fileName);
+
+      if (!fs.existsSync(pathImage)) {
+        pathImage = path.join(process.cwd(), 'dist', 'uploads', 'no-image.png');
+      }
+
+      const imageBuffer = fs.readFileSync(pathImage).toString('base64');
+
+      return {
+        imageBuffer,
+        contentType: 'image/jpeg',
+      };
+
+    } catch (error) {
+      throw new RpcException({
+        status: 500,
+        message: error.message || 'An error occurred while fetching the image.',
+      });
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const {
+        rut,
+        first_name,
+        second_name,
+        paternal_lastname,
+        maternal_lastname,
+        email,
+        password,
+        creator_user,
+        roleId
+      } = createUserDto;
+
+      const user = await this.users.findFirst({
+        where: {
+          OR: [
+            { rut },
+            { email }
+          ]
+        }
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.users.create({
+        data: {
+          rut,
+          first_name,
+          second_name,
+          paternal_lastname,
+          maternal_lastname,
+          email,
+          password: bcrypt.hashSync(password, 10),
+          creator_user,
+          roleId,
+        },
+      });
+
+      const { password: __, ...rest } = newUser;
+
+      const serializedUser = {
+        ...rest,
+        name: `${first_name} ${paternal_lastname}`,
+        roleId: Number(roleId),
+      };
+
+      return {
+        user: serializedUser,
+        token: await this.singJWT(serializedUser),
+      };
+
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async update(id: string, type: string, file?: Express.Multer.File) {
+    try {
+      // validate image type
+      const validImageTypes = ['users', 'persons'];
+
+      if (!validImageTypes.includes(type)) {
+        throw new RpcException({
+          status: 400,
+          message: 'Invalid image type.',
+        });
+      }
+
+      // check if the file is valid
+      if (file && Buffer.isBuffer(file.buffer)) {
+        // process image
+        const cutName = file.originalname.split('.');
+        const extensionFile = cutName[cutName.length - 1];
+
+        //validate extension
+        const validExtensions = ['png', 'jpg', 'jpeg', 'gif'];
+
+        if (!validExtensions.includes(extensionFile)) {
+          throw new RpcException({
+            status: 415,
+            message: 'Unsupported Media Type: Invalid image extension.',
+          });
+        }
+
+        // generate file name
+        const fileName = `${uuidv4()}.${extensionFile}`;
+
+        // path to save the image
+        const uploadsPath = path.join(__dirname, '..', 'uploads', type);
+
+        if (!fs.existsSync(uploadsPath)) {
+          fs.mkdirSync(uploadsPath, { recursive: true });
+        }
+
+        const filePath = path.join(uploadsPath, fileName);
+        fs.writeFileSync(filePath, file.buffer);
+
+        // update image in database
+        await this.updateImage(id, type, fileName);
+
+        return {
+          status: 201,
+          message: 'file loaded successfully',
+          file_name: fileName
+        };
+      } else {
+        throw new RpcException({
+          status: 400,
+          message: 'No file uploaded',
+        });
+      }
+    } catch (error) {
+      throw new RpcException({
+        status: 500,
+        message: error.message || 'An error occurred while loading an image.',
+      });
+    }
+  }
+
+  async updateImage(id: string, type: string, fileName: string) {
+    let oldPath = '';
+
+    switch (type) {
+      case 'users':
+        const user = await this.users.findUnique({
+          where: { id },
+          select: {
+            image: true
+          }
+        });
+
+        if (!user) {
+          return false;
+        }
+
+        oldPath = `${process.cwd()}/dist/uploads/users/${user.image}`;
+        this.deleteImage(oldPath);
+
+        await this.users.update({
+          where: { id },
+          data: { image: fileName }
+        });
+
+        return true;
+        break;
+      case 'persons':
+        const person = await this.users.findUnique({
+          where: { id },
+          select: {
+            image: true
+          }
+        });
+
+        if (!person) {
+          return false;
+        }
+
+        oldPath = `${process.cwd()}/dist/uploads/persons/${person.image}`;
+        this.deleteImage(oldPath);
+
+        await this.users.update({
+          where: { id },
+          data: { image: fileName }
+        });
+
+        return true;
+        break
+    }
+  }
+
+  deleteImage(path: string) {
+    if (fs.existsSync(path)) {
+      // delete old image
+      fs.unlinkSync(path);
+    }
   }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
+  convertBigInts(obj: any) {
+    return JSON.parse(
+      JSON.stringify(obj, (key, value) =>
+        typeof value === 'bigint' ? value.toString() : value
+      )
+    );
+  }
+
+
 }
