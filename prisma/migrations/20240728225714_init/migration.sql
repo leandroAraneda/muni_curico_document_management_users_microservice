9871e02e5202645972714fb047f0082bbe5f0190
Luis Araneda <leandro.araneda@hotmail.com>
2024-11-05 01:47:34 -0300

    feat: schema.prisma updated

--- prisma/migrations/20240728225714_init/migration.sql
+++ prisma/migrations/20240728225714_init/migration.sql
@@ -1,10 +1,15 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `lastname` VARCHAR(191) NOT NULL,
+    `rut` VARCHAR(12) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `second_name` VARCHAR(191) NOT NULL,
+    `paternal_lastname` VARCHAR(191) NOT NULL,
+    `maternal_lastname` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(150) NULL,
     `password` VARCHAR(191) NOT NULL,
+    `creator_user` VARCHAR(191) NOT NULL,
     `status` BOOLEAN NOT NULL DEFAULT true,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
